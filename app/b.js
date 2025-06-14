6bcb8a84ae63a761f874e0bf50b270d6399bf027
Ei Maung <eimg@fairwayweb.com>
2025-06-14 16:52:00 +0630

    todo

---
+++ app/b.js
@@ -0,0 +1,6 @@
+// b.js - CommonJS Module - CJS
+
+const { PI, add } = require("./a.js");
+
+console.log( PI );
+console.log( add(1, 2) );
---
+++ app/b.mjs
@@ -0,0 +1,6 @@
+// b.mjs - ES6 Module - ESM
+
+import { PI, add } from "./a.mjs";
+
+console.log( PI );
+console.log( add(2, 3) );
