57285befbea3c83ecaef7b2f647fa6d4922f988b
Ei Maung <eimg@fairwayweb.com>
2025-08-02 17:00:45 +0630

    categories client

--- blog/routes/api.php
+++ blog/routes/api.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CategoryController;
+
+Route::apiResource('/categories', CategoryController::class);
+
+// Route::get('/categories', [CategoryController::class, 'index']);
+// Route::post('/categories', [CategoryController::class, 'store']);
+// Route::get('/categories/{id}', [CategoryController::class, 'show']);
+// Route::put('/categories/{id}', [CategoryController::class, 'update']);
+// Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
