e8cbfb286683d5c1a7692bf47d466af1f3217a08
Ei Maung <eimg@fairwayweb.com>
2025-08-09 09:28:29 +0630

    update

--- blog/app/Http/Controllers/Api/CategoryController.php
+++ blog/app/Http/Controllers/Api/CategoryController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware("auth:sanctum");
+    }
+
     public function index()
     {
         return Category::all();
--- blog/routes/api.php
+++ blog/routes/api.php
@@ -3,14 +3,28 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoryController;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 Route::apiResource('/categories', CategoryController::class);
 
-// Route::get('/categories', [CategoryController::class, 'index']);
-// Route::post('/categories', [CategoryController::class, 'store']);
-// Route::get('/categories/{id}', [CategoryController::class, 'show']);
-// Route::put('/categories/{id}', [CategoryController::class, 'update']);
-// Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+Route::post('/login', function(Request $request) {
+    $email = $request->email;
+    $password = $request->password;
+
+    if($email == "" or $password == "") {
+        return response(['msg' => 'email and password required'], 400);
+    }
+
+    $user = User::where("email", $email)->first();
+    if($user) {
+        if(Hash::check($password, $user->password)) {
+            return $user->createToken("api")->plainTextToken;
+        }
+    }
+
+    return response(['msg' => 'invalid email or password'], 401);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
